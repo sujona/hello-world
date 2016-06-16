1fd8d53514e714b1f11ce9ad0618b1fd71ceaa8a
SU/jona <jonatan.ostrom@fysik.su.se>
2016-06-16 14:28:33 +0200

    Update test.f90

--- test.f90
+++ test.f90
@@ -1,6 +1,7 @@
 program hej
 write(*,*) "does this editor have code highlighting"
 write(*,*) "i would like that"
+k=0 !i like implicit variables
 end program
 
 !why is there highlighting in the overview but not in the editor?
